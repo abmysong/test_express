cf991859e97617afb9da875f145b23ed87f0cd30
abmysong <abmysong@gmail.com>
2022-11-09 20:58:03 +0900

    [#3] boards read

--- routes/boards.js
+++ routes/boards.js
@@ -12,9 +12,24 @@ router.post('/', jwtAuth.tokenCheck, function(req, res) {
   `;
   db.query(sql, [req.decoded.member_pk, req.body.title, req.body.content], function(error, rows) {
     if (!error || db.error(req, res, error)) {
-      console.log('Done members join', rows);
+      console.log('Done boards created', rows);
       res.status(200).send({
         result: 'Created'
+      });
+    }
+  });
+});
+
+router.get('/', function(req, res) {
+  const sql = `
+    select b.*, m.id from test_boards b inner join test_members m on b.member_pk = m.member_pk;
+  `;
+  db.query(sql, [], function(error, rows) {
+    if (!error || db.error(req, res, error)) {
+      console.log('Done boards read', rows);
+      res.status(200).send({
+        result: 'Read',
+        boards: rows
       });
     }
   });

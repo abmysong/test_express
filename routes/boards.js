cf9a180e753997d610d1642558c0c6196c1b8a81
abmysong <abmysong@gmail.com>
2022-11-16 20:39:13 +0900

    [#3] boards update, delete

--- routes/boards.js
+++ routes/boards.js
@@ -35,4 +35,54 @@ router.get('/', function(req, res) {
   });
 });
 
+router.get('/:board_pk', function(req, res) {
+  const board_pk = req.params.board_pk;
+  const sql = `
+    select b.*, m.id from test_boards b inner join test_members m on b.member_pk = m.member_pk
+    where board_pk = ?;
+  `;
+  db.query(sql, [board_pk], function(error, rows) {
+    if (!error || db.error(req, res, error)) {
+      console.log('Done boards detail', rows);
+      res.status(200).send({
+        result: 'Read',
+        board: rows[0]
+      });
+    }
+  });
+});
+
+router.delete('/:board_pk', function(req, res) {
+  const board_pk = req.params.board_pk;
+  const sql = `
+    delete from test_boards
+    where board_pk = ?;
+  `;
+  db.query(sql, [board_pk], function(error, rows) {
+    if (!error || db.error(req, res, error)) {
+      console.log('Done boards delete', rows);
+      res.status(200).send({
+        result: 'Deleted'
+      });
+    }
+  });
+});
+
+router.put('/:board_pk', function(req, res) {
+  const board_pk = req.params.board_pk;
+  const sql = `
+    update test_boards
+    set title = ?, content = ?
+    where board_pk = ?;
+  `;
+  db.query(sql, [req.body.title, req.body.content, board_pk], function(error, rows) {
+    if (!error || db.error(req, res, error)) {
+      console.log('Done boards update', rows);
+      res.status(200).send({
+        result: 'Updated'
+      });
+    }
+  });
+});
+
 module.exports = router;
